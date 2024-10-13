d445fe52558a2f087b99f4deaf2303fb6f62154a
Nick Kocharhook <nick@kocharhook.com>
2024-10-13 18:12:00 -0400

    Fix "unique key" React error

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import IconButton from '@mui/material/IconButton';
 import Paper from '@mui/material/Paper';
 import { styled } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
-import Typography from '@mui/material/Typography';
 import { getCookie, getCookies, setCookie } from 'cookies-next';
 import { QRCodeSVG } from 'qrcode.react';
 import { useEffect, useState } from 'react';
--- src/replaceTemplateWithJSX.js
+++ src/replaceTemplateWithJSX.js
@@ -1,4 +1,5 @@
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
+import Typography from '@mui/material/Typography';
 import React from "react";
 import Link from '/src/Link';
 
@@ -7,6 +8,7 @@ export default function replaceTemplateWithJSX(template, replacements) {
   let result = [];
   let lastIndex = 0;
   let match;
+  var i = 0;
 
   while ((match = regex.exec(template)) !== null) {
     const placeholder = match[0];
@@ -14,14 +16,14 @@ export default function replaceTemplateWithJSX(template, replacements) {
     var replacement = undefined;
 
     if (replacements.hasOwnProperty(key)) {
-      replacement = replacements[key];
+      replacement = <Typography component='span' key={i++}>{replacements[key]}</Typography>;
     } else if (placeholder.startsWith("https://")) {
-      replacement = <><Link href={placeholder} target="_blank">{placeholder}<OpenInNewIcon sx={{ fontSize: "14px", verticalAlign: "middle", marginLeft: "2px" }} /></Link></>;
+      replacement = <Link key={i++} href={placeholder} target="_blank">{placeholder}<OpenInNewIcon sx={{ fontSize: "14px", verticalAlign: "middle", marginLeft: "2px" }} /></Link>;
     }
 
     // Add string before the placeholder
     if (match.index > lastIndex) {
-      result.push(template.substring(lastIndex, match.index));
+      result.push(<Typography component='span' key={i++}>{template.substring(lastIndex, match.index)}</Typography>);
     }
 
     if (replacement !== undefined) {
@@ -35,7 +37,7 @@ export default function replaceTemplateWithJSX(template, replacements) {
 
   // Add any remaining string after the last placeholder
   if (lastIndex < template.length) {
-    result.push(template.substring(lastIndex));
+    result.push(<Typography component='span' key={i++}>{template.substring(lastIndex)}</Typography>);
   }
 
   return result;
